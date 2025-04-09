d5ffde4bef860aadbbb34c4f79dfb2a1a027c4d7
Marcelo Sassi <marcelosassi@gmail.com>
2025-04-09 16:03:57 -0300

    fix: remove homepage e usa base dinâmica para deploy Render/GH

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/dash-2/',
+export default defineConfig(({ mode }) => {
+  // Carrega variáveis de ambiente específicas do modo (development, production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/', // Usa variável de ambiente ou '/' como padrão
+  }
 })
